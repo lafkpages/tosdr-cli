4e436d726b6996f81f1a4c3bb2eeefa9c3090718
lafkpages <luisafk@proton.me>
2023-11-01 19:10:21 +0100

    Use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    domain: String,
+}
 
 mod structs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        println!("Usage: tosdr <domain>");
-        return;
-    }
+    let args = Args::parse();
 
     let resp = ureq::get(&format!(
         "https://api.tosdr.org/search/v4/?query={}",
-        args[1]
+        args.domain
     ))
     .call()
     .unwrap()
     .into_json::<structs::Resp>()
     .unwrap();
 
-    println!("Results for \"{}\":", args[1]);
+    println!("Results for \"{}\":", args.domain);
     for service in resp.parameters.services {
         println!("  - {} ({})", service.name, service.id);
         println!("    - {}", service.rating.human);
