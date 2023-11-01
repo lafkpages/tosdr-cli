fe0289eeb00a947fde93430fd85736146ff33f9e
lafkpages <luisafk@proton.me>
2023-11-01 19:25:45 +0100

    Allow either --domain or --query but not both

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,40 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    query: String,
+struct CliArgs {
+    #[clap(flatten)]
+    args: CliArgsGroup,
+}
+
+#[derive(Parser, Debug)]
+#[group(required = true, multiple = false)]
+struct CliArgsGroup {
+    #[clap(short, long)]
+    query: Option<String>,
+
+    #[clap(short, long)]
+    domain: Option<String>,
 }
 
 mod structs;
 
 fn main() {
-    let args = Args::parse();
+    let cli = CliArgs::parse();
 
-    let resp = ureq::get(&format!(
-        "https://api.tosdr.org/search/v4/?query={}",
-        args.query
-    ))
-    .call()
-    .unwrap()
-    .into_json::<structs::Resp>()
-    .unwrap();
+    println!("CLI: {:#?}", cli);
 
-    println!("Results for \"{}\":", args.query);
+    let query = cli
+        .args
+        .query
+        .unwrap_or(unsafe { cli.args.domain.unwrap_unchecked() });
+
+    let resp = ureq::get(&format!("https://api.tosdr.org/search/v4/?query={}", query))
+        .call()
+        .unwrap()
+        .into_json::<structs::Resp>()
+        .unwrap();
+
+    println!("Results for \"{}\":", query);
     for service in resp.parameters.services {
         println!("  - {} ({})", service.name, service.id);
         println!("    - {}", service.rating.human);
