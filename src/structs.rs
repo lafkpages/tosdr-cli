5df067f934b890d961c6ba73fc55a2c38bf38bf7
lafkpages <luisafk@proton.me>
2023-11-02 10:38:05 +0100

    Links structs

--- src/structs.rs
+++ src/structs.rs
@@ -21,6 +21,7 @@ pub struct RespService {
     pub urls: Vec<String>,
     pub wikipedia: String,
     pub rating: RespServiceRating,
+    pub links: RespServiceLinks,
 }
 
 #[derive(Deserialize, Debug)]
@@ -28,4 +29,31 @@ pub struct RespServiceRating {
     pub hex: u8,
     pub human: String,
     pub letter: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RespServiceLinks {
+    pub crisp: RespServiceCrispLinks,
+    pub phoenix: RespServicePhoenixLinks,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RespServiceCrispLinks {
+    pub api: String,
+    pub service: String,
+    pub badge: RespServiceBadge,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RespServiceBadge {
+    pub svg: String,
+    pub png: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RespServicePhoenixLinks {
+    pub service: String,
+    pub documents: String,
+    pub new_comment: String,
+    pub edit: String,
 }
